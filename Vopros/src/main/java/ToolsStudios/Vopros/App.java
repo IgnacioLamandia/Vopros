b5c888ee9321a8be9d3a5c89100ac1c609da3b96
Ignacio Lamandia <ignaciolamandia@gmail.com>
2017-09-29 12:56:11 -0300

    Se creo el proyecto Vopros

---
+++ Vopros/src/main/java/ToolsStudios/Vopros/App.java
@@ -0,0 +1,13 @@
+package ToolsStudios.Vopros;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
