13bb51b134ce9ef5fa822b40b89cea353d0c1d42
Ignacio Lamandia <ignaciolamandia@gmail.com>
2017-11-02 19:41:14 -0300

    Intento de vista de la expiracion de Issue

--- src/main/webapp/js/controllerNuevoIssue.js
+++ src/main/webapp/js/controllerNuevoIssue.js
@@ -18,6 +18,12 @@ app.controller('NuevoIssueCtrl', function($resource,$state,$stateParams,Issue,$t
     'MEDIA',
     'ALTA'];
 
+	self.days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+	self.months = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+	self.years = [2017,2018,2019,2020,2021,2022,2023,2024,2025];
+
     self.users = [];
 
     this.getUsers= function(){
