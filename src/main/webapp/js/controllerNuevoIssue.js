0fb9323d3a24d9896a345cd05da0ddaa3923ccaa
Gaston Veliez <gastonveliez95@gmail.com>
2017-11-03 02:00:37 -0300

    La fecha en expiracion ahora por default es la actual

--- src/main/webapp/js/controllerNuevoIssue.js
+++ src/main/webapp/js/controllerNuevoIssue.js
@@ -3,6 +3,11 @@ app.controller('NuevoIssueCtrl', function($resource,$state,$stateParams,Issue,$t
 
     var self = this;
 
+    self.currentdate = new Date(); 
+	self.datetime = this.currentdate.getFullYear()+"-"+(this.currentdate.getMonth()+1)  + "-" 
+				+this.currentdate.getDate() 
+                ;
+
     self.issue= {"titulo":"","tipo":"","gravedad":"","prioridad":"", "expiracion":"", "asignado":""};
 
     self.tipo=[ 'BUG',
@@ -18,17 +23,10 @@ app.controller('NuevoIssueCtrl', function($resource,$state,$stateParams,Issue,$t
     'MEDIA',
     'ALTA'];
 
-    self.fecha;
-
-	self.days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-
-	self.months = [1,2,3,4,5,6,7,8,9,10,11,12];
-
-	self.years = [2017,2018,2019,2020,2021,2022,2023,2024,2025];
 
     self.users = [];
 
-    self.inputfecha = document.createElement("INPUT");
+    self.inputfecha = document.getElementById("fecha");
 
 
     this.getUsers= function(){
@@ -42,15 +40,16 @@ app.controller('NuevoIssueCtrl', function($resource,$state,$stateParams,Issue,$t
 
     this.inputFecha=function() {
     
-    this.inputfecha.setAttribute("type", "date");
-    this.inputfecha.setAttribute("value", "2017-01-01");
-    document.getElementById("fecha").append(this.inputfecha);
+
+    console.log(this.datetime);
+    this.inputfecha.setAttribute("value", this.datetime);
+    this.inputfecha.value = this.datetime;
 }
 
 	this.inputFecha();
 
    this.asignarFecha=function() {
-    this.issue.expiracion= this.inputfecha.value;
+    this.issue.expiracion= this.inputfecha.value.substring(0,10);
 }
 
 
