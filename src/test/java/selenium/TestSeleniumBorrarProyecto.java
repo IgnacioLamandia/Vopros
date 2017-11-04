f0d5e5467279f0049341c2f93cbc462308d592ba
Gaston Veliez <gastonveliez95@gmail.com>
2017-11-03 22:26:26 -0300

    Test borrar proyecto implementado

--- src/test/java/selenium/TestSeleniumBorrarProyecto.java
+++ src/test/java/selenium/TestSeleniumBorrarProyecto.java
@@ -1,24 +1,47 @@
 package selenium;
 
-import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestSeleniumBorrarProyecto {
 
-	@Before
-	public void setUp() throws Exception {
+private static WebDriver driver= null;
+	
+	@BeforeClass
+	public static void inicializarDirver() {
+//        System.setProperty("webdriver.gecko.driver","/home/gaston/Documentos/geckodriver"); 
+//		driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver","/home/gaston/Documentos/chromedriver");
+		driver = new ChromeDriver();
+		
+	}
+	
+	@AfterClass
+	public static void exitDriver() {
+		driver.quit();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+	@Test(expected=NoSuchElementException.class)
+	public void testBorrarProyecto() {
+		driver.get("http://localhost:8080/#!/main/proyectos");
+		WebElement proyecto= driver.findElement(By.id("Vopros"));
+		proyecto.click();
+		WebElement boton = driver.findElement(By.id("borrar"));
+		boton.click();
+		new WebDriverWait(driver, 5).until(ExpectedConditions.invisibilityOfElementLocated(By.id("Vopros")));
+		//Chequeo que elemento ya no exista y dispare excepcion
+		driver.findElement(By.id("Vopros"));
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
