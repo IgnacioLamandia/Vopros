78a366b1e0a5a60ede0f9d7db165c3c31cac80c0
Ignacio Gioya <ignacio.gioya@gmail.com>
2017-11-20 16:41:38 -0300

    Se agrego el metodo que trae la conversacion entre dos usuarios

--- src/main/java/tk/vopros/backend/service/ChatService.java
+++ src/main/java/tk/vopros/backend/service/ChatService.java
@@ -1,5 +1,7 @@
 package tk.vopros.backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,11 @@ public class ChatService {
 	@Transactional
 	public void setMensaje(Mensaje mensaje){
 		mensajeDAO.save(mensaje);
+	}
+	
+	@Transactional
+	public List<Mensaje> getConversacion(Long emisorId, Long receptorId) {
+		return mensajeDAO.getMensajes(emisorId,receptorId);		
 	}
 
 }
