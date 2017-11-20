4f0d94aba5703539b9560e777a448585b5cb8f1d
Ignacio Gioya <ignacio.gioya@gmail.com>
2017-11-20 17:05:23 -0300

    Se elimino un import

--- src/main/java/tk/vopros/frontend/api/ChatController.java
+++ src/main/java/tk/vopros/frontend/api/ChatController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import tk.vopros.backend.model.Mensaje;
 import tk.vopros.backend.model.User;
