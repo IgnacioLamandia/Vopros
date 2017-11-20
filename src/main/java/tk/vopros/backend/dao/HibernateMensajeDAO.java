a6c76bbb37d682e5c42dc573451e88ce0678242b
Ignacio Gioya <ignacio.gioya@gmail.com>
2017-11-20 16:41:12 -0300

    Se agrego el metodo que trae la conversacion entre dos usuarios

--- src/main/java/tk/vopros/backend/dao/HibernateMensajeDAO.java
+++ src/main/java/tk/vopros/backend/dao/HibernateMensajeDAO.java
@@ -1,12 +1,39 @@
 package tk.vopros.backend.dao;
 
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import tk.vopros.backend.model.Mensaje;
+import tk.vopros.backend.model.User;
 
 @Repository
 public class HibernateMensajeDAO extends GenericDAO<Mensaje> {
 	public HibernateMensajeDAO() {
 		super(Mensaje.class);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public List<Mensaje> getMensajes(Long emisorId, Long receptorId) {
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			String hql = "from " + "Mensaje " + 
+						    "m " + "where (m.emisor.id = :emisorId AND m.receptor.id = :receptorId) "
+								 + "   OR (m.emisor.id = :receptorId AND m.receptor.id = :emisorId)";
+			Query<Mensaje> query = session.createQuery(hql, Mensaje.class);
+			query.setParameter("emisorId", emisorId);
+			query.setParameter("receptorId", receptorId);
+			return query.list();
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			throw new RuntimeException(e);
+		} finally {
+			session.close();
+		}
+		
 	}
 }
