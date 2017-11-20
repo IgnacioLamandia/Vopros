8d84260d3ecbd2de6a023b6be0b2733f41ca659b
Ignacio Gioya <ignacio.gioya@gmail.com>
2017-11-20 15:45:40 -0300

    Se agregaron pavadas sin funcionalidad

--- src/main/webapp/js/controllerChat.js
+++ src/main/webapp/js/controllerChat.js
@@ -1,5 +1,12 @@
-app.controller('ChatCtrl', function() {
+app.controller('ChatCtrl', function($stateParams) {
 	'use strict';
 
     var self = this;
+    this.usuarios = $stateParams.miembros;
+    this.conversacion = []
+    var Conversacion = null;
+
+    this.verConversacion = function(usuarioId){
+        
+    }
 });
